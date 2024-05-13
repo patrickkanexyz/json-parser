e76b5ec04cae71a76aadd93c1ac95a0517470610
Patrick Kane <patrick@patrickkane.xyz>
2024-05-12 17:30:46 -0700

    reads from STDIN

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+    "io"
+    "os"
+    "log"
 )
 
 func main() {
 	fmt.Println("Hello JSON.")
+
+    stdin, err := io.ReadAll(os.Stdin)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("%s\n", stdin)
+
+    data := string(stdin[:])
+
+    for index, char := range data {
+        fmt.Printf("Index: %d Char: %q\n", index, char)
+    }
 }
